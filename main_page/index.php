85c8542acefdfd55ceb062a1b5f2d5905b9485de
Robi <robi.bouwens@bazandpoort.be>
2023-10-26 09:26:08 +0200

    update footer2

--- main_page/index.php
+++ main_page/index.php
@@ -1,7 +1,13 @@
 <?php 
 include "../components/navbar.php";  
-
-
-
 ?>
-
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>title</title>
+</head>
+<body>
+    
+</body>
+</html>
