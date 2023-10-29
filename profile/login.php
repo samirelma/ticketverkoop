c3a20b3fb614c819f2076ecd8ca908a54394f955
Robi <134525322+Robi1106@users.noreply.github.com>
2023-10-29 09:53:12 +0100

    Update login.php

--- profile/login.php
+++ profile/login.php
@@ -8,7 +8,14 @@ include $_SERVER['DOCUMENT_ROOT'] . "/components/navbar.php";
     <title>title</title>
 </head>
 <body>
-    <a href="../profile/register.php">registreren</a>
+<label class="label">gebruikersnaam</label>
+<input type="text" placeholder="gebruikersnaam" class="input input-bordered input-primary w-full max-w-xs" /> <br>  
+<label class="label">wachtwoord</label>
+<input type="password" placeholder="wachtwoord" class="input input-bordered input-primary w-full max-w-xs" /><br>
+
+
+
+<a href="../profile/register.php">registreren</a>
 </body>
 </html>
 <?php
