f4f99238db5a237a00ffa9291ec5eb71fcd98bfb
Robi <134525322+Robi1106@users.noreply.github.com>
2023-10-29 10:10:45 +0100

    Update login.php

--- profile/login.php
+++ profile/login.php
@@ -8,14 +8,24 @@ include $_SERVER['DOCUMENT_ROOT'] . "/components/navbar.php";
     <title>title</title>
 </head>
 <body>
+<?php
+if (isset($_POST["knop"])){
+
+} else {
+echo '
+<form method="post" action="login.php">
 <label class="label">gebruikersnaam</label>
-<input type="text" placeholder="gebruikersnaam" class="input input-bordered input-primary w-full max-w-xs" /> <br>  
+<input type="text" placeholder="gebruikersnaam" class="input input-bordered input-primary w-full max-w-xs" name="gebruikersnaam" /> <br>  
 <label class="label">wachtwoord</label>
-<input type="password" placeholder="wachtwoord" class="input input-bordered input-primary w-full max-w-xs" /><br>
+<input type="password" placeholder="wachtwoord" class="input input-bordered input-primary w-full max-w-xs" name="wachtwoord" /><br>
+<button class="btn" type="submit" name="knop">login</button><br>
+</form>
 
 
-
-<a href="../profile/register.php">registreren</a>
+<a href="../profile/register.php" >als je nog geen account hebt, <br> klik hier om te Registreren </a>
+';
+}
+?>
 </body>
 </html>
 <?php
