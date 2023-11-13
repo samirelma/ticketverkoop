ca4492421e9a12d5aa3b254a1337bda63e7de7c6
samirelma <samir.elmazzouji@bazandpoort.be>
2023-11-13 10:00:12 +0100

    still need fixing

--- profile/evenementen-toevoegen.php
+++ profile/evenementen-toevoegen.php
@@ -40,7 +40,10 @@ if (isset($_POST['create'])) {
   );
 }
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 function addEvent(
   $naam,
   $datum,
@@ -49,6 +52,7 @@ function addEvent(
   $file
 ) {
     $query = 'INSERT INTO evenementen (naam, datum, aantalTickets, beschrijving, afbeelding) VALUES (?, ?, ?, ?, ?)';
+<<<<<<< Updated upstream
 
     $imageName = $file['name'];
     $imageTmpName = $file['tmp_name'];
@@ -60,9 +64,34 @@ function addEvent(
   
 
     // execute the query
+=======
+    
+
+    // Set the image name to the uploaded file name or to 'default.png' if no file is uploaded
+    $imageName = isset($file['afbeelding']) ? $file['afbeelding'] : 'default.png';
+
+    // Set the temporary image name to the uploaded file's temporary name or to null if no file is uploaded
+    $imageTmpName = isset($file['tmp_name']) ? $file['tmp_name'] : null;
+
+    // If a file is uploaded, move it to the public downloads directory and update the image name
+    if ($imageTmpName) {
+        $targetDir = PUBLIC_R . "/Downloads/";
+        $baseImageName = basename($imageName, ".png") . ".png";
+        $targetFile = $targetDir . $baseImageName;
+        move_uploaded_file($imageTmpName, $targetFile);
+    } else {
+        // Use a default image if no image is uploaded
+        $imageName = 'default.png';
+    }
+
+    // Create a new mysqli object to connect to the database
+>>>>>>> Stashed changes
     $mysqli = new mysqli('localhost', 'root', '', 'dbticketverkoop'); 
 
+    // Prepare the SQL query
     $stmt = $mysqli->prepare($query);
+
+    // Bind the parameters to the prepared statement
     $stmt->bind_param(
         'ssiss',    
         $naam,
@@ -126,15 +155,17 @@ function addEvent(
 
     <!-- Image -->
     <div class="form-control w-full">
-        <label class="label">
-            <span class="label-text text-purple-500">Foto</span>
-        </label>
-        <input name="afbeelding" type="file" class="file-input file-input-bordered w-full" required />
-    </div>
+    <label class="label">
+      <span class="label-text text-purple-500">Foto</span>
+    </label>
+    <input type="file" name="fileToUpload" id="fileToUpload">
+  </div>
+
 
     <div class="form-control w-full max-w-xs mt-4">
         <button name="create" class="btn btn-primary bg-blue-500 hover:bg-blue-600 text-white">Create</button>
     </div>
+    
     <br>
 </form>
 </form> 
