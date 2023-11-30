7658768657239a1ef6f99503dacc62809d8caf63
Robi <134525322+Robi1106@users.noreply.github.com>
2023-11-30 21:59:17 +0100

    Update generatePDF.php

--- generatePDF.php
+++ generatePDF.php
@@ -21,6 +21,7 @@ foreach ($zaalgegevens as $zaal);
 
 $userID = $tickets["userID"];
 $userdata = getUserDataByID($mysqli, $userID);
+foreach ($userdata as $user);
 //generates pdf + add page
 $pdf = new FPDF(); 
 $pdf -> AddPage(); 
@@ -29,7 +30,12 @@ $pdf -> AddPage();
 $pdf->SetFont('Arial', 'B', 10);
 $pdf -> Cell(50, 10, 'RS Ticket Service', 0, 0, 'C');
 $pdf -> Cell(100, 10, $evenement["naam"], 0, 1, 'C');
-$pdf -> Image("./img/eventPictures/" . $evenement['afbeelding'] . "",10,20, 190, 110);
+$pdf -> Cell(190,110,$pdf -> Image("./img/eventPictures/" . $evenement['afbeelding'] . "",10,20, 190, 110),1,1, 'C');
+
+$pdf ->SetFont('Arial', '', 10); 
+$pdf -> Cell(50, 10, 'naam: '.$user["lastname"].' '.$user["firstname"], 1,0, 'L');
+$pdf ->Cell(50,20, 'plaats: '.$tickets["seatName"], 1, 0, 'C'); 
+$pdf -> Cell(50, 10, 'email: '.$user["email"], 1,1, 'L');
 
 
 //saves the pdf as file + output to browser 
