978036df07f1590ec577a98ce2489edf4cd6e96b
Robi <robi.bouwens@bazandpoort.be>
2023-12-04 15:54:07 +0100

    pdf ticket

--- generatePDF.php
+++ generatePDF.php
@@ -5,6 +5,7 @@ include $_SERVER['DOCUMENT_ROOT'] . "/connect/db.php";
 include $_SERVER['DOCUMENT_ROOT'] . "/fetch/util.php";
 session_start();
 
+
 $userid = $_SESSION["gebruikersid"];
 $ticketid = $_GET["ticketID"];
 
@@ -25,6 +26,12 @@ foreach ($userdata as $user);
 
 $categorieData = getCategorieDataByID($mysqli, $tickets["categoryID"]); 
 foreach ($categorieData as $categorie); 
+
+if ($categorie["name"] == "Golden Cirkel (VIP)") {
+ $kleineLetters = "Op vertoon van dit ticket krijg je toegang tot het eten, de zaal en de parking. De deuren van de voorstelling gaan telkens een half uur voor de start van de voorstelling open.\n Met dit ticket kan je maar een keer binnen geef deze ticket dus niet door zonder dit te registreren op de website."; 
+} else {
+    $kleineLetters = "Op vertoon van dit ticket krijg je toegang tot de zaal en de parking. De deuren van de voorstelling gaan telkens een half uur voor de start van de voorstelling open. Met dit ticket kan je maar een keer binnen geef deze ticket dus niet door zonder dit te registreren op de website.";
+}
 //generates pdf + add page
 $pdf = new FPDF(); 
 $pdf -> AddPage(); 
@@ -37,9 +44,10 @@ $pdf -> SetTextColor(253,255,255);
 $pdf -> Cell(50, 10, 'naam', 1,0, 'L',1);
 $pdf -> cell(50,10,'ticketnummer', 1, 0, 'L', 1); 
 $pdf -> Cell(90,10,'datum van voorstelling', 1, 1, 'L', 1);
-$pdf -> Cell(50,10,$user['lastname'].' '. $user['firstname'], 1, 0, 'L',1);
-$pdf -> Cell(50,10,$tickets['TicketID'],1,0,'L',1); 
-$pdf -> Cell(90,10,$evenement['datum'],1,1,'L',1); 
+$pdf -> SetTextColor(0,0,0);
+$pdf -> Cell(50,10,$user['lastname'].' '. $user['firstname'], 1, 0, 'L');
+$pdf -> Cell(50,10,$tickets['TicketID'],1,0,'L'); 
+$pdf -> Cell(90,10,$evenement['datum'],1,1,'L'); 
 
 $pdf -> Cell(190,110,$pdf -> Image("./img/eventPictures/" . $evenement['afbeelding'] . "",10,40, 190, 110),0,1, 'C');
 
@@ -58,16 +66,12 @@ $pdf -> SetTextColor(0,0,0);
 $pdf -> Cell(100,15,$evenement["beschrijving"],1,0, 'L'); 
 $pdf -> Cell (45,15, $tickets["rij"] ,1,0, 'L',0); 
 $pdf -> Cell(45,15,$tickets["stoel"] ,1,1,'L',0);
-$pdf -> Cell(190,10,'',0,1,'L'); 
+// white space invoegen 
+$pdf -> Ln(10); 
 $pdf -> Cell(70,70,'qr code', 1, 0, 'C');
-$pdf -> Cell(120,70,$categorie["beschrijving"],1,1,'L', 0);
+$pdf->MultiCell( 120, 10, $kleineLetters, 1, 'L',0);
 
-
-
-
-
-//saves the pdf as file + output to browser 
-//$pdf-> Output('F', 'ticket.pdf'); 
+// output to browser 
 $pdf-> Output('i'); 
 
 ?>
