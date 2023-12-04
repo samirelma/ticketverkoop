fd8b22026cfbbccde780891c7e255d144a43ea5b
samirelma <samir.elmazzouji@bazandpoort.be>
2023-12-04 09:30:40 +0100

    fixed gegevens aanpassen

--- profile/gegevens_aanpassen.php
+++ profile/gegevens_aanpassen.php
@@ -16,52 +16,30 @@ include $_SERVER['DOCUMENT_ROOT'] . "/components/navbar.php";
 $mysqli = new mysqli('localhost', 'root', '', 'dbticketverkoop');
 
 // Fetch the current user's data
-$query = 'SELECT * FROM users WHERE username = ?';
+$query = 'SELECT * FROM users WHERE id = ?';
 $stmt = $mysqli->prepare($query);
-$stmt->bind_param('i', $_SESSION['id']); // 's' stands for string
+$stmt->bind_param('i', $_SESSION["gebruikersid"]);
 $stmt->execute();
 $result = $stmt->get_result();
 $data = $result->fetch_assoc();
-echo 'id: ' . $_SESSION['id'];
-echo'<br>';
 
 
-if (isset($_SESSION['id'])) {
-    echo 'id: ' . $_SESSION['id'];
-} else {
-    echo 'id is not set.';
+
+// If the user clicked the submit button...
+if (isset($_POST['wijzig'])) {
+    // Update the record
+    $query = 'UPDATE users SET firstname = ?, lastname = ?, email = ?, username = ? WHERE id = ?';
+    $stmt = $mysqli->prepare($query);
+    $stmt->bind_param('ssssi', $_POST['firstname'], $_POST['lastname'], $_POST['email'], $_POST['username'], $_SESSION["gebruikersid"]);
+    $stmt->execute();
+
+
+    // Redirect to this page
+    header('Location: /profile/gegevens_aanpassen.php');
 }
-echo'<br>';
-// Handle form submission
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['username'], $_POST['email'], $_POST['firstname'], $_POST['lastname'])) {
-        $query = 'UPDATE users SET username = ?, email = ?, firstname = ?, lastname = ? WHERE id = ?';
-        $stmt = $mysqli->prepare($query);
-        $stmt->bind_param('ssssi', $_POST['username'], $_POST['email'], $_POST['firstname'], $_POST['lastname'], $_SESSION['id']);
 
-        if ($stmt->execute()) {
-            // Fetch the updated data
-            $query = 'SELECT * FROM users WHERE id = ?';
-            $stmt = $mysqli->prepare($query);
-            $stmt->bind_param('i', $_SESSION['id']);
-            if ($stmt->execute()) {
-                $result = $stmt->get_result();
-                if ($result) {
-                    $updatedData = $result->fetch_assoc();
-                    if ($updatedData) {
-                        // rest of your code
-                    } else {
-                        echo 'No id found with the given id.';
-                    }
-                } else {
-                    echo 'Failed to execute query. Error: ' . $stmt->error;
-                }
-            } else {
-                echo 'Failed to bind parameters. Error: ' . $stmt->error;
-            }
-        }
-    }
-}
+
+
 ?>
 
 <!-- Your HTML code here -->
@@ -77,14 +55,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         <label class="label">
                             <span class="label-text">Firstname</span>
                         </label>
-                        <input type="text" name="firstname" value="<?php echo isset($data['firstname']) ? $data['firstname'] : ''; ?>" class="input input-bordered w-full" required />
+                        <input type="text" name="firstname" value="<?php echo ($data['firstname']) ?>" class="input input-bordered w-full" required />
+
                     </div>
 
                     <div class="form-control md:flex-1">
                         <label class="label">
                             <span class="label-text">Lastname</span>
                         </label>
-                        <input type="text" name="lastname" value="<?php echo isset($data['lastname']) ? $data['lastname'] : ''; ?>" class="input input-bordered w-full" required />
+                        <input type="text" name="lastname" value="<?php echo ($data['lastname']) ?>" class="input input-bordered w-full" required />
                     </div>
                 </div>
 
@@ -93,15 +72,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         <label class="label">
                             <span class="label-text">Email</span>
                         </label>
-                        <input type="email" name="email" value="<?php echo isset($data['email']) ? $data['email'] : ''; ?>" class="input input-bordered w-full" required />
+                        <input type="email" name="email" value="<?php echo ($data['email']) ?>" class="input input-bordered w-full" required />
                     </div>
 
                     <div class="form-control md:flex-1">
                         <label class="label">
                             <span class="label-text">Username</span>
                         </label>
-                        <input type="text" name="username" value="<?php echo isset($data['username']) ? $data['username'] : ''; ?>" class="input input-bordered w-full" required />
+                        <input type="text" name="username" value="<?php echo ($data['username']) ?>" class="input input-bordered w-full" required />
                     </div>
+                </div>
+                <!-- Image -->
+                <div class="form-control w-full">
+                    <label class="label">
+                        <span class="label-text text-blue-500">Foto</span>
+                    </label>
+                    <input type="file" name="fileToUpload" id="fileToUpload">
                 </div>
                 <button name="wijzig" class="btn btn-primary">Wijzig gegevens</button>
         </form>
