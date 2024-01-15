3b79d6aa69e6aae342158be05331c9a6db4a94e6
Robi <robi.bouwens@bazandpoort.be>
2024-01-15 15:35:48 +0100

    add overzichtevenementen

--- components/navbar.php
+++ components/navbar.php
@@ -55,6 +55,7 @@ $searchTerm = $_GET['search'] ?? '';
         <li><a href="../profile/logout.php">Uitloggen</a></li>';         
           if ($_SESSION["user"] == "bedrijf") { 
             echo '<li><a href="../profile/evenementen-toevoegen.php">evenementen toevoegen</a></li>';
+            echo'<li><a href="../overzichtEvenementen.php">overzicht evenementen</a></li>'; 
           }
           if ($_SESSION["user"] == "member") {
             echo '<li><a href="../profile/mijnTickets.php">Mijn Tickets</a></li>';
