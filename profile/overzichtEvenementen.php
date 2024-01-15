a678b465a3ad937b3e79073f0ce56f094bb38e92
Robi <robi.bouwens@bazandpoort.be>
2024-01-15 16:06:29 +0100

    weergeven evenementenoverzicht bedrijf

--- profile/overzichtEvenementen.php
+++ profile/overzichtEvenementen.php
@@ -1,3 +1,6 @@
+<?php
+ include $_SERVER['DOCUMENT_ROOT'] . "/components/navbar.php";
+ ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,6 +8,46 @@
     <title>title</title>
 </head>
 <body>
-    
+    <?php
+   
+   ?>
+   <div class="overflow-x-auto">
+   <table class="table">
+     <thead>
+       <tr>
+         <th></th>
+         <th>concert</th>
+         <th>beschrijving</th>
+         <th>weergeven</th>
+       </tr>
+     </thead>
+     <tbody>
+       <?php
+       $userID = $_SESSION["gebruikersid"]; 
+        $dataevenement = getEventsByUserID($mysqli,$userID); 
+       foreach ($dataevenement as $evenemten) {
+       ?>
+           <tr>
+             <th><?php echo $evenemten["evenementID"]?></th>
+             <td><?php echo $evenemten["naam"]?></td>
+             <td><?php echo $evenemten["beschrijving"]?></td>
+             <?php
+             echo '
+              <form method="post" action="overzichtEvenementen.php">
+              <td><input type="checkbox" class="toggle toggle-info" checked /></td>
+              </form>
+             '?>
+           </tr>
+         </tbody>
+       </table>
+       <?php
+       }
+       ?>
+     </div>
+
 </body>
 </html>
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . "/components/footer.php";
+?>
+
