820ad7981316cbd2fd60ce3407d05a2d9876932d
Robi <robi.bouwens@bazandpoort.be>
2024-01-19 16:22:48 +0100

    Update overzichtEvenementen.php

--- profile/overzichtEvenementen.php
+++ profile/overzichtEvenementen.php
@@ -24,23 +24,32 @@
      <tbody>
        <?php
        $userID = $_SESSION["gebruikersid"]; 
-        $dataevenement = getEventsByUserID($mysqli,$userID); 
-       foreach ($dataevenement as $evenemten) {
+        $dataEvenement = getEventsByUserID($mysqli,$userID); 
+       foreach ($dataEvenement as $evenemten) {
+        if ($evenemten["weergeven"] == 1) {
+          $checked = "checked"; 
+        } else {
+          $checked = ""; 
+        }
        ?>
+       
            <tr>
              <th><?php echo $evenemten["evenementID"]?></th>
              <td><?php echo $evenemten["naam"]?></td>
              <td><?php echo $evenemten["beschrijving"]?></td>
-             <?php
-             echo '
+             
+             <?php echo '
               <form method="post" action="overzichtEvenementen.php">
-              <td><input type="checkbox" class="toggle toggle-info" checked /></td>
+              <td><input type="checkbox" class="toggle toggle-info" id="weergeven" '.$checked.'/></td>
               </form>
-             '?>
+             '; ?>
            </tr>
          </tbody>
        </table>
        <?php
+       }
+       if(isset($_POST["weergeven"])) {
+        
        }
        ?>
      </div>
