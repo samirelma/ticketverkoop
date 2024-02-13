dc7f1d863db4f768f0e78f26e1fb5971601a2ce1
Robi <134525322+Robi1106@users.noreply.github.com>
2024-02-13 12:02:36 +0100

    Create selectTicketPage.php

---
+++ selectTicketPage.php
@@ -0,0 +1,16 @@
+<?php
+ include $_SERVER['DOCUMENT_ROOT'] . "/components/navbar.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>title</title>
+</head>
+<body>
+    
+</body>
+</html>
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . "/components/footer.php";
+?>
