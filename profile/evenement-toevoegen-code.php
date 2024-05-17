ba56f51cfafbf9771a98861570db8ca5251ba442
samirelma <samir.elmazzouji@bazandpoort.be>
2024-05-17 15:03:14 +0200

    Update evenement-toevoegen-code.php

--- profile/evenement-toevoegen-code.php
+++ profile/evenement-toevoegen-code.php
@@ -1,5 +1,5 @@
 <?php
-define('PUBLIC_R', '/path/to/your/directory');
+define('PUBLIC_R', $_SERVER['DOCUMENT_ROOT']);
 //make it sp if u arent logged in u cant get to this page and if u arent admin or bedrijf u cant get to this page
 if (!isset($_SESSION['user'])) {
     header("Location: /profile/login.php");
