6f13e1e285ba1fcadf2516143a8f15d43ba35e60
samirelma <samir.elmazzouji@bazandpoort.be>
2024-06-02 16:34:19 +0200

    make the font bigger

--- validate.php
+++ validate.php
@@ -4,7 +4,18 @@ include $_SERVER['DOCUMENT_ROOT'] . "/connect/connect.php";
 include $_SERVER['DOCUMENT_ROOT'] . "/connect/db.php";
 include $_SERVER['DOCUMENT_ROOT'] . "/fetch/util.php";
 
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Validate Ticket</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css">
+    
+</head>
+
+<?php
 if (!isset($_GET['ticketID'])) {
     echo "No ticket ID provided";
     exit;
@@ -20,15 +31,15 @@ $stmt->execute();
 $result = $stmt->get_result();
 
 if ($result->num_rows == 0) {
-    echo "Invalid Ticket";
+    echo '<div class="text-6xl font-bold text-center text-red-500 bg-blue-500 rounded-lg p-6 shadow-lg">Invalid Ticket Date</div>';
     exit;
 }
 
 // Check if the ticket is already scanned
 $ticket = $result->fetch_assoc();
 if ($ticket['scanned'] == 1) {
-    echo "Ticket already used";
-    exit;
+    echo '<div class="text-6xl font-bold text-center text-red-500 bg-blue-500 rounded-lg p-6 shadow-lg">Ticket already used</div>';
+        exit;
 }
 $dataTickets = getTicketData($mysqli, $ticketid); 
 foreach ($dataTickets as $tickets); 
@@ -43,7 +54,7 @@ $eventDate = $evenement["datum"];
 $eventDate = date("Y-m-d", strtotime($eventDate));
 
 if ($eventDate != $currentDate) {
-    echo "Invalid Ticket Date";
+    echo '<div class="text-6xl font-bold text-center text-red-500 bg-blue-500 rounded-lg p-6 shadow-lg">Invalid Ticket Date</div>';
     exit;
 }
 
@@ -53,5 +64,5 @@ $stmt = $mysqli->prepare($sql);
 $stmt->bind_param('i', $ticketid);
 $stmt->execute();
 
-echo "Valid Ticket";
+echo '<div class="text-6xl font-bold text-center text-green-500 bg-blue-500 rounded-lg p-6 shadow-lg">Valid Ticket</div>';
 ?> 
